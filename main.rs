4f0ab18a87df87352f6d355fc532ed47890ede44
racoci <racoci.0@gmail.com>
2022-07-24 17:26:23 -0300

    Testando ownership (propriedade), pattern matching, e erros.

--- main.rs
+++ main.rs
@@ -52,6 +52,11 @@ fn main() {
 
     condicionais();
     repeticoes();
+
+    ownership();
+
+    pattern_matching();
+    erros();
 }
 
 fn condicionais() {
@@ -109,4 +114,40 @@ fn repeticoes() {
     for i in 1..11 {
         println!("{} x {} = {}", multiplicador, i, multiplicador * i);
     }
+}
+
+fn ownership() {
+    let mut uma_string = String::from("Vinicius");
+    rouba(&mut uma_string);
+
+    println!("{}", uma_string);
+}
+
+fn rouba(string: &mut String) {
+    string.push_str(" Dias");
+    println!("{}", string);
+}
+
+fn pattern_matching() {
+    for x in 1..=20 {
+        println!("{}: {}", x, match x {
+            1 => "Pouco",
+            2 | 3 => "Um pouquinho",
+            4..=10 => "Um bocado",
+            _ if x % 2 == 0 => "Uma boa quantidade",
+            _ => "Muito"
+        });
+    }
+}
+
+fn erros() {
+    match resultado() {
+        Ok(s) => println!("String de sucesso = {}", s),
+        Err(numero) => println!("Codigo de erro = {}", numero)
+    };
+}
+
+fn resultado() -> Result<String, u8>
+{
+    Err(42)
 }
